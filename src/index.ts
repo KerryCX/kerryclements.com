5a6a7d5f5390f72650bca8da81d2fc9f0ca10941
Kerry Clements <kerryclements@Kerrys-MacBook-Pro.local>
2025-09-09 20:53:18 +0100

    started adding some animations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import elements from './listOfElements.json' with { type: 'json' };
+import elements from './listOfElements.json' with { type: 'json' }
 
 document.addEventListener('DOMContentLoaded', () => {
-  let randomIndex: number;
-  const elementSymbol = document.getElementById('element-symbol');
-  const getSymbolButton = document.getElementById('button');
-  const answer = document.getElementById('answer');
-  const symbolOrigin = document.getElementById('symbol-origin');
-  const reasonForNaming = document.getElementById('reason-for-naming');
-  const answerButton = document.getElementById('answer-button');
+  let randomIndex: number
+  const elementSymbol = document.getElementById('element-symbol')
+  const getSymbolButton = document.getElementById('button')
+  const answer = document.getElementById('answer')
+  const symbolOrigin = document.getElementById('symbol-origin')
+  const reasonForNaming = document.getElementById('reason-for-naming')
+  const answerButton = document.getElementById('answer-button')
 
   if (
     !elementSymbol ||
@@ -17,36 +17,38 @@ document.addEventListener('DOMContentLoaded', () => {
     !symbolOrigin ||
     !answerButton
   ) {
-    console.error('something not found!');
-    return;
+    console.error('something not found!')
+    return
   }
 
-  getSymbolButton.addEventListener('click', () => {
-    randomIndex = Math.floor(Math.random() * elements.length);
-    const randomElement = elements[randomIndex]; // Access the element using the index
-    if (randomElement) {
-      elementSymbol.textContent = randomElement.symbol;
-    }
-    answerButton.classList.remove('hidden');
-    answer.textContent = '';
-    reasonForNaming.textContent = '';
-    symbolOrigin.textContent = '';
-    getSymbolButton.textContent = 'Click for Next';
-  });
-  answerButton.addEventListener('click', () => {
-    const randomElement = elements[randomIndex];
-    if (randomElement) {
-      answer.textContent = randomElement?.name;
-      // symbolOrigin.textContent = randomElement?.symbolOrigin
-    }
+  let isNameVisible = false
 
-    reasonForNaming.classList.remove('hidden');
-    reasonForNaming.textContent = 'Why?';
-  });
-  reasonForNaming.addEventListener('click', () => {
-    const randomElement = elements[randomIndex];
+  getSymbolButton.addEventListener('click', () => {
+    randomIndex = Math.floor(Math.random() * elements.length)
+    const randomElement = elements[randomIndex] // Access the element using the index
     if (randomElement) {
-      symbolOrigin.textContent = randomElement?.symbolOrigin;
+      elementSymbol.textContent = randomElement.symbol
+      elementSymbol.classList.add('visible')
+      answerButton.classList.add('visible')
+      answer.classList.remove('visible')
+      reasonForNaming.classList.remove('visible')
     }
-  });
-});
+    symbolOrigin.textContent = ''
+    getSymbolButton.textContent = 'Click for Next'
+  })
+  answerButton.addEventListener('click', () => {
+    const randomElement = elements[randomIndex]
+    if (randomElement) {
+      answer.textContent = randomElement?.name
+      answer.classList.add('visible')
+    }
+    reasonForNaming.classList.add('visible')
+  })
+  reasonForNaming.addEventListener('click', () => {
+    const randomElement = elements[randomIndex]
+    if (randomElement) {
+      symbolOrigin.textContent = randomElement?.symbolOrigin
+      symbolOrigin.classList.add('visible')
+    }
+  })
+})
