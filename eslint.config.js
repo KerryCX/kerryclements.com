15caee08c1618efe74f18e05c8168aa05a11896a
Kerry Clements <kerryclements@me.com>
2026-09-24 15:00:34 +0100

    chore: update ESLint config for v10 and react-hooks 7

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,13 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'build']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
